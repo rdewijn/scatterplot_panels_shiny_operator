d742df0c2679226c2e0e9751618f4e512cc2c0e9
Rik <rdewijn@xs4all.nl>
2021-03-14 13:25:58 +0000

    refactored fixed error when no color supplied added option to set sizes added option to (un)wrap the panel grid added option axis equal - free between panels

--- ui.R
+++ ui.R
@@ -1,15 +1,17 @@
 library(shiny)
 
-shinyUI(fluidPage(
+ui <- shinyUI(fluidPage(
   
-  titlePanel("Scatterplot"),
+  titlePanel("Boxplot"),
   
   sidebarPanel(
     sliderInput("plotWidth", "Plot width (px)", 200, 2000, 500),
     sliderInput("plotHeight", "Plot height (px)", 200, 2000, 500),
     textInput("xlab", "x axis label", ""),
     textInput("ylab", "y axis label", ""),
-    checkboxInput("labs", "Add text labels")
+    checkboxInput("labs", "Apply labels",0),
+    checkboxInput("wrap", "Wrap panel grid", 0),
+    checkboxInput("fixed", "Axes equal for panels",0)
   ),
   
   mainPanel(
--- workspace.R
+++ workspace.R
@@ -4,13 +4,14 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(ggrepel)
+library(ggsci)
 set.seed(42)
 
 ############################################
 #### This part should not be included in ui.R and server.R scripts
 getCtx <- function(session) {
-  ctx <- tercenCtx(stepId = "309ec088-89c7-448d-bef6-5ad611f3357e",
-                   workflowId = "01cb95cd7b746443ed9f40625200ef4f")
+  ctx <- tercenCtx(workflowId = "8f17d834dda49eba43ac822ed600aa7b",
+                   stepId = "706ac7cf-8dd6-41a9-83a7-b5802ea67031")
   return(ctx)
 }
 ####
@@ -25,7 +26,9 @@ ui <- shinyUI(fluidPage(
     sliderInput("plotHeight", "Plot height (px)", 200, 2000, 500),
     textInput("xlab", "x axis label", ""),
     textInput("ylab", "y axis label", ""),
-    checkboxInput("labs", "Add text labels")
+    checkboxInput("labs", "Apply labels",0),
+    checkboxInput("wrap", "Wrap panel grid", 0),
+    checkboxInput("fixed", "Axes equal for panels",0)
   ),
   
   mainPanel(
@@ -50,63 +53,63 @@ server <- shinyServer(function(input, output, session) {
   
   output$main.plot <- renderPlot({
     
-    values <- dataInput()
-    
-    df <- values$data
-    in.col <- values$colors
-    in.lab <- values$labels
-    
-    input.par <- list(
-      xlab = input$xlab,
-      ylab = input$ylab,
-      labs = input$labs
-    )
+    df <- dataInput()
 
-    fill.col <- NULL
-    if(length(unique(in.col)) > 0) fill.col <- as.factor(in.col)
-    labels <- NULL
-    if(length(unique(in.lab)) > 0) labels <- as.factor(in.lab)
+    plt = ggplot(df, aes(x = .x, y = .y, colour = colors, label = labels)) +
+      labs(x = input$xlab, y = input$ylab)
+
+    if(input$labs & !all(is.na(df$labels))) {
+      plt <- plt + geom_point() + geom_text_repel()
+    }
+    else if(input$labs & !all(is.na(df$sizes))){
+      plt = plt + geom_point(aes(size = sizes))
+    }
+    else{
+      plt = plt + geom_point()
+    }
     
-    theme_set(theme_minimal())
-    print(labels)
-    plt <- ggplot(df, aes(x = .x, y = .y, fill = fill.col, label = labels)) + 
-      geom_point() + labs(x = input.par$xlab, y = input.par$ylab, fill = "Legend")
-    
-    if(input.par$labs) plt <- plt + geom_text_repel()
-    if(!is.null(df$cnames)) plt <- plt + facet_wrap(~ cnames + rnames)
-    
+    if (!input$wrap){
+      plt <- plt + facet_grid(rnames ~ cnames, scales = ifelse(input$fixed, "fixed", "free"))
+    } else {
+      plt = plt + facet_wrap(~ rnames + cnames, scales = ifelse(input$fixed, "fixed", "free"))
+    }
+    plt = plt + theme_bw()
     plt
-    
   })
   
 })
 
 getValues <- function(session){
-  
+  #browser()
   ctx <- getCtx(session)
-  
-  values <- list()
-  
-  values$data <- ctx %>% select(.x, .y, .ri, .ci) %>%
+  df <- ctx %>% select(.x, .y, .ri, .ci) %>%
     group_by(.ri)
-  
-  values$colors <- NA
-  if(length(ctx$colors)) values$colors <- ctx$select(ctx$colors[[1]])[[1]]
 
-  values$labels <- NA
-  if(length(ctx$labels)) values$labels <- as.character(ctx$select(ctx$labels[[1]])[[1]])  
+  colors <- 0
+  if(length(ctx$colors)) colors <- ctx$select(ctx$colors[[1]])[[1]]
+  if(!all(is.numeric(colors))) colors = as.factor(colors)
   
-  values$rnames <- ctx$rselect()[[1]]
-  names(values$rnames) <- seq_along(values$rnames) - 1
-  values$data$rnames <- values$rnames[as.character(values$data$.ri)]
+  labels <- NA
+  sizes = NA
+  if(length(ctx$labels)) tcn.labels <- ctx$select(ctx$labels[[1]])[[1]]
+  if (!all(is.numeric(tcn.labels))) {
+    labels = as.factor(tcn.labels)
+  } else {
+    sizes = tcn.labels
+  }
   
-  if(nchar(values$rnames) == 0) values$data$rnames <- values$colors
   
-  values$cnames <- ctx$cselect()[[1]]
-  names(values$cnames) <- seq_along(values$cnames) - 1
-  values$data$cnames <- values$cnames[as.character(values$data$.ci)]
+  df = df %>% data.frame(labels, colors, sizes)
   
-  return(values)
+  cnames = ctx$cselect()[[1]]
+  
+  rnames = ctx$rselect()[[1]]
+  df = df %>% 
+    mutate(colors = colors, labels = labels) %>%
+    left_join(data.frame(.ri = 0:(length(rnames)-1), rnames), by = ".ri") %>%
+    left_join(data.frame(.ci = 0:(length(cnames)-1), cnames), by = ".ci")
+  
+  return(df)
 }
 
 runApp(shinyApp(ui, server))  
